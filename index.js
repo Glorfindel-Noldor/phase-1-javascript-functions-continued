c2337ba63468ebbfedd17557e14edd5507fcd949
Bogar Martinez <bogar.cm@icloud.com>
2023-06-08 20:57:58 -0500

    yay

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(acty='roller-skate'){
+    return `This Saturday, I want to ${acty}!`;
+}
+function mondayWork(acty='go to the office'){
+    return `This Monday, I will ${acty}.`;
+}
+
+
+
+
+function wrapAdjective(x="*"){
+    return function(msg='special'){
+        return x === "*" ? `You are ${x}a hard worker${x}!` : `You are ${x}a dedicated programmer${x}!`;
+}
+}
+const encouragingPromptFuncion = wrapAdjective(x)
+
+
